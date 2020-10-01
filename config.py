7432e5d1c15380c3e24e6877e1580877d89eff75
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2020-10-01 14:32:00 +0100

    Auto commit on behalf of Apollo Akora at 2020-10-01 14:31:59 +0100

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     RABBITMQ_VHOST = os.getenv('RABBITMQ_VHOST', '/')
     RABBITMQ_EXCHANGE = os.getenv('RABBITMQ_EXCHANGE', '')
     RABBITMQ_USER = os.getenv('RABBIT_USER', 'rmquser')
-    RABBITMQ_PASSWORD = os.getenv('RABBIT_PASSWORD', 'rmqp455w0rd')
+    RABBITMQ_PASSWORD = os.getenv('RABBIT_PASSWORD', 'qpassword')
     DB_USERNAME = os.getenv('DB_USERNAME', 'postgres')
     DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
