e88c4bdade11772b502f56a5a08aa05d7f6398ea
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-11-19 08:28:45 +0000

    hacking around

--- rabbit_spammer.py
+++ rabbit_spammer.py
@@ -16,33 +16,33 @@ def parse_arguments():
 
 def spam(queue, duration):
     end_time = datetime.utcnow() + timedelta(0, 0, 0, 0, float(duration))
-    message = json.dumps(
-        {
-            "message": "This is to spam",
-        })
+    # message = json.dumps(
+    #     {
+    #         "message": "This is to spam",
+    #     })
 
-    # message = json.dumps({
-    #     "event": {
-    #         "type": "SURVEY_LAUNCHED",
-    #         "source": "CONTACT_CENTRE_API",
-    #         "channel": "CC",
-    #         "dateTime": f"{datetime.utcnow().isoformat()}Z",
-    #         "transactionId": str(uuid.uuid4())
-    #     },
-    #     "payload": {
-    #         "response": {
-    #             "questionnaireId": '034343434r34',
-    #             "caseId":  str(uuid.uuid4()),
-    #             "agentId": "cc_000351"
-    #         }
-    #     }
-    # })
+    message = json.dumps({
+        "event": {
+            "type": "SURVEY_LAUNCHED",
+            "source": "CONTACT_CENTRE_API",
+            "channel": "CC",
+            "dateTime": f"{datetime.utcnow().isoformat()}Z",
+            "transactionId": str(uuid.uuid4())
+        },
+        "payload": {
+            "response": {
+                "questionnaireId": '034343434r34',
+                "caseId":  str(uuid.uuid4()),
+                "agentId": "cc_000351"
+            }
+        }
+    })
 
-    with RabbitContext(exchange='') as rabbit:
+    with RabbitContext(exchange='events') as rabbit:
         while datetime.utcnow() < end_time:
             rabbit.publish_message(
                 message=message,
-                content_type='application/json', headers='', routing_key='Case.Responses')
+                content_type='application/json', headers='', routing_key='event.response.authentication')
 
 
 def main():
